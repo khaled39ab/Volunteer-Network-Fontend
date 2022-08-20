a278782315df6b066e043d6dcb874350a84c55ac
khaled39ab <khaled39ab@gmail.com>
2022-08-20 23:48:36 +0600

    handle google social login

--- src/pages/Shared/SocialLogIn/SocialLogIn.js
+++ src/pages/Shared/SocialLogIn/SocialLogIn.js
@@ -2,11 +2,20 @@ import React from 'react';
 import googleIcon from './../../../images/Google.png'
 import facebookIcon from './../../../images/Facebook.png'
 import './SocialLogIn.css'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const SocialLogIn = () => {
+    const [
+        signInWithGoogle,
+        user,
+        loading,
+        error
+    ] = useSignInWithGoogle(auth);
+
     return (
         <div className='login-container'>
-            <div className='social-login border rounded-pill'>
+            <div onClick={() => { signInWithGoogle() }} className='social-login border rounded-pill'>
                 <img src={googleIcon} alt="" />
                 <p>Continue with Google</p>
             </div>
