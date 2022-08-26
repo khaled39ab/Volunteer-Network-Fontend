036fde48f286a83beca9905a83c938b66477ef85
khaled39ab <khaled39ab@gmail.com>
2022-08-26 21:35:37 +0600

    some changes

--- src/pages/Event/Event.js
+++ src/pages/Event/Event.js
@@ -3,14 +3,19 @@ import { Card } from 'react-bootstrap';
 import './Event.css'
 
 const Event = ({ event }) => {
-    const { name, img } = event;
+    const { _id, name, img } = event;
+
+    const handleAddEvent = (id) => {
+        console.log(id);
+    }
+
     return (
         <>
             <div className='event'>
                 <Card>
                     <Card.Img variant="top" src={img} />
-                    <Card.Body className='card-title'>
-                        <Card.Title>{name} <span>Click to Join</span></Card.Title>
+                    <Card.Body className='card-title' onClick={() => handleAddEvent(_id)}>
+                        <Card.Title className='card-title-text'>{name}</Card.Title>
                     </Card.Body>
                 </Card>
             </div>
